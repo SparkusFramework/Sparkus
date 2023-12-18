4d22ef748e87aa8b12e776affca6786efd131dc8
Maxandre Rochefort <contact@maxand.re>
2023-12-18 13:35:30 +0100

    Refactor logger message formatting and add timestamp (#9)
    * Refactor logger message formatting and add timestamp
    
    The logger in the core of the application has been modified to include a timestamp in each log message. This provides better visibility into the timing of events. The logger now uses the `color` function to format the log messages, which helps in distinguishing log levels and other relevant information. The `color` function has been modified to accept multiple color parameters, allowing for more complex color combinations. The `time` function has been added to generate a timestamp in ISO format with bold styling. These changes improve the readability and aesthetic appeal of the log messages.
    
    * Rebuild

--- dist/utils/logger.mjs
+++ dist/utils/logger.mjs
@@ -7,14 +7,34 @@ export var SparkusLoggerLevel;
 })(SparkusLoggerLevel || (SparkusLoggerLevel = {}));
 var Color;
 (function (Color) {
-    Color["BLUE"] = "\u001B[36m";
-    Color["RED"] = "\u001B[31m";
-    Color["GRAY"] = "\u001B[90m";
-    Color["YELLOW"] = "\u001B[33m";
+    // Foregrounds
+    Color["WHITE"] = "\u001B[38;5;231m";
+    Color["BLACK"] = "\u001B[38;5;232m";
+    Color["BLUE"] = "\u001B[38;5;33m";
+    Color["PURPLE"] = "\u001B[38;5;99m";
+    Color["ORANGE"] = "\u001B[38;5;208m";
+    Color["GREEN"] = "\u001B[38;5;41m";
+    Color["RED"] = "\u001B[38;5;204m";
+    Color["GRAY"] = "\u001B[38;5;243m";
+    // Backgrounds
+    Color["BG_WHITE"] = "\u001B[48;5;231m";
+    Color["BG_BLUE"] = "\u001B[48;5;33m";
+    Color["BG_PURPLE"] = "\u001B[48;5;99m";
+    Color["BG_ORANGE"] = "\u001B[48;5;208m";
+    Color["BG_RED"] = "\u001B[48;5;204m";
+    Color["BG_GREEN"] = "\u001B[48;5;41m";
+    Color["BG_GRAY"] = "\u001B[48;5;243m";
+    // Specials
+    Color["BOLD"] = "\u001B[1m";
     Color["RESET"] = "\u001B[0m";
 })(Color || (Color = {}));
-function color(message, color) {
-    return `${color}${message}${Color.RESET}`;
+function color(message, ...color) {
+    const colorsString = color.reduce((previous, current) => `${previous}${current}`, "");
+    return `${colorsString}${message}${Color.RESET}`;
+}
+function time() {
+    const now = new Date();
+    return color(`${now.toISOString()}`, Color.BOLD);
 }
 export class Logger {
     static level = SparkusLoggerLevel.DEBUG;
@@ -24,22 +44,34 @@ export class Logger {
     }
     debug(message) {
         if (Logger.level <= SparkusLoggerLevel.DEBUG) {
-            console.log(color(`[DEBUG] [${this.name}] `, Color.GRAY) + message);
+            console.log(time() + " :: " +
+                color(`DEBUG`, Color.BOLD) + " " +
+                color(`${this.name}`, Color.GRAY, Color.BOLD) + " :: " +
+                color(message, Color.GRAY));
         }
     }
     info(message) {
         if (Logger.level <= SparkusLoggerLevel.INFO) {
-            console.log(color(`[INFO] [${this.name}] `, Color.BLUE) + message);
+            console.log(time() + " :: " +
+                color(` INFO `, Color.BG_PURPLE, Color.WHITE, Color.BOLD) + " " +
+                color(`${this.name}`, Color.PURPLE, Color.BOLD) + " :: " +
+                message);
         }
     }
     warn(message) {
         if (Logger.level <= SparkusLoggerLevel.WARN) {
-            console.log(color(`[WARN] [${this.name}] `, Color.YELLOW) + message);
+            console.log(time() + " :: " +
+                color(` WARN `, Color.BG_ORANGE, Color.WHITE, Color.BOLD) + " " +
+                color(`${this.name}`, Color.BOLD, Color.ORANGE) + " :: " +
+                color(message, Color.ORANGE));
         }
     }
     error(message, error) {
         if (Logger.level <= SparkusLoggerLevel.ERROR) {
-            console.log(color(`[ERROR] [${this.name}] ${message}`, Color.RED));
+            console.log(time() + " :: " +
+                color(` ERROR `, Color.BG_RED, Color.WHITE, Color.BOLD) + " " +
+                color(`${this.name}`, Color.BOLD, Color.RED) + " :: " +
+                color(message, Color.RED));
             if (error) {
                 console.error(error);
             }
